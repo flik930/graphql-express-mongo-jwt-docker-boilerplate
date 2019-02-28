acc030601da522e18cdc238edb97e3bc8c52a780
Steve Lam <steve.lam@gravitysupplychain.com>
2019-02-28 16:50:50 +0800

    fix import

--- graphql-server/resolvers.js
+++ graphql-server/resolvers.js
@@ -4,6 +4,8 @@ import {
   GraphQLDateTime
 } from 'graphql-iso-date';
 
+import User from 'models/User';
+
 export default {
   Query: {
     me: async (_, args, context) => await User.findById(context.user._id),
