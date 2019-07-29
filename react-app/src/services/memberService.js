d4c18c6ef5a1301368bc9f73bf74010fe0cc6b70
Steve Lam <steve.lam@gravitysupplychain.com>
2019-07-29 18:11:21 +0800

    get gql client on call

--- react-app/src/services/memberService.js
+++ react-app/src/services/memberService.js
@@ -2,15 +2,13 @@ import config from '../config';
 import {Get, Post, Put, Delete, getGqlClient} from './baseService';
 import { gql } from "apollo-boost";
 
-const gqlClient = getGqlClient();
-
 const MemberService = {
   emailLogin: (params) => Post({url: config.baseUrl + 'login', params}),
   signup: (params) => Post({url: config.baseUrl + 'signup', params}),
   facebookLogin: (params) => Post({url: config.baseUrl + 'auth/facebook', params}),
   forgotPassword: (params) => Post({url: config.baseUrl + 'forgot', params}),
   resetPassword: (params) => Post({url: config.baseUrl + 'reset', params}),
-  getUserInfo: () => gqlClient.query({
+  getUserInfo: () => getGqlClient().query({
     query: gql`
       {
         me {
